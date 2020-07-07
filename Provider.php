6e7719681fe98b38f3e8c54cd9f01202fdd94cff
Titus Kirch <titus.kirch@tkirch.dev>
2020-07-07 18:04:03 +0200

    Create Provider.php

---
+++ Provider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SocialiteProviders\OpenAuth;
+
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'OPENAUTH';
+}
