da464e6d98942cd26789b77b6dc268442bd02d3c
Titus Kirch <titus.kirch@tkirch.dev>
2020-07-07 23:30:56 +0200

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -11,4 +11,67 @@ class Provider extends AbstractProvider
      * Unique Provider Identifier.
      */
     const IDENTIFIER = 'OPENAUTH';
+
+    /**
+     * @inheritdoc
+     */
+    protected $scopes = [
+        'email',
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://openauth.dev/oauth2-authorize/',
+            $state
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://openauth.dev/oauth2-token/';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://openauth.dev/open-id-user-information/',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'email'    => $user['email'],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
 }
